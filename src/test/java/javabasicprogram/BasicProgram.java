c7abe9c1feb65189d3e8068b474151f28200299a
Suresh Upreti <sureshupreti877@gmail.com>
2026-02-22 19:00:41 +0545

    add basic program

---
+++ src/test/java/javabasicprogram/BasicProgram.java
@@ -0,0 +1,11 @@
+package javabasicprogram;
+
+public class BasicProgram {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a+b;
+
+        System.out.println("The sum is: "+sum);
+    }
+}
